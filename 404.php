e51288d7516afff0b7d5a85a1be73bdee5f0e3ae
aovejas <alvaro@byadrenaline.com>
2016-02-09 17:20:37 +0100

    SCSS changes

--- 404.php
+++ 404.php
@@ -3,9 +3,11 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
 			
 		<div class="wrapper">
+
 			<h1><?php _e('(Error 404)', 'byadr'); ?></h1>	
 			<p><?php _e('UPPS!! No hemos encontrado lo que buscabas.', 'byadr'); ?></p>
 			<a href="<?php echo home_url( ); ?>" class="btn">Vuelve a la página inicial</a>
+			
 		</div>
 
 	</article>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<?php if( have_posts() ): ?>
 
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
-			<div class="wrapper">
+			
 			<?php while( have_posts() ):the_post(); ?>
 
 				<h2><?php the_title(); ?></h2>
@@ -12,7 +12,7 @@
 
 
 			<?php endwhile; ?>
-			</div>
+			
 		</article>
 
 	<?php endif; ?>
